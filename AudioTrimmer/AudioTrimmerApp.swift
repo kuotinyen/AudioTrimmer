3810532790fd5ed43b853bfb88a89cc46d2f50d3
TK <kuotinyen@gmail.com>
2025-10-07 21:51:27 +0800

    Init code

---
+++ AudioTrimmer/AudioTrimmerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioTrimmerApp.swift
+//  AudioTrimmer
+//
+//  Created by Ting-Yen, Kuo on 2025/10/7.
+//
+
+import SwiftUI
+
+@main
+struct AudioTrimmerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioTrimmer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AudioTrimmer
+//
+//  Created by Ting-Yen, Kuo on 2025/10/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
